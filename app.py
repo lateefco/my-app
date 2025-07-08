cd810a8fd820b1e14f02581ea4d0b9d25d6de8e9
Lateef Jawando <lateef.jawando@mongodb.com>
2025-07-08 14:17:58 +0100

    adding an app secret

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
-# 
+    text = "{}<br><br>Here's my secret: {}".format(
+        os.environ.get('APP_MESSAGE', 'Hello, World!'),
+        os.environ.get('APP_SECRET')
+    )
+    return text
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
